675bd3bb9b5e92fc8b2455f27e38d8f30ea5703d
DINH TRAN <147835496+puudynk05@users.noreply.github.com>
2026-04-05 20:04:16 +0700

    feat: update frontend signup page and project configs

--- source/frontend/src/App.tsx
+++ source/frontend/src/App.tsx
@@ -1,8 +1,13 @@
 import SignInPage from "./pages/SignInPage"
 import { BrowserRouter, Routes, Route } from 'react-router'
+import SignUpPage from "./pages/SignUpPage";
+import ChatAppPage from "./pages/ChatAppPage";
+import { Toaster } from "sonner";
 
 function App() {
-  return <>
+  return 
+  <>
+  <Toaster richColors/>
   <BrowserRouter>
     <Routes>
       {/* public routes */}
@@ -10,8 +15,17 @@ function App() {
         path='/signin'
         element={<SignInPage/>}
       />
+      <Route 
+        path='/signup'
+        element={<SignUpPage/>}
+      />
 
-      {/* private routes */}
+      {/* protected routes */}
+      {/* todo: tạo protested route */}
+      <Route 
+        path='/'
+        element={<ChatAppPage/>}
+      />
     </Routes>
   </BrowserRouter>
   </>;
--- source/frontend/src/pages/SignUpPage.tsx
+++ source/frontend/src/pages/SignUpPage.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { SignupForm } from '../components/signup-form'
 
 const SignUpPage = () => {
   return (
-    <div>SignUpPage</div>
+        <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
+      <div className="w-full max-w-sm md:max-w-4xl">
+        <SignupForm />
+      </div>
+    </div>
   )
 }
 

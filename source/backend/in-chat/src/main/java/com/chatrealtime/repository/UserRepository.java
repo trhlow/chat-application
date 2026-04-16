c80176722befada27ba842a265279ab152c09a83
TrHlow <thlong190705@gmail.com>
2026-04-16 19:35:26 +0700

    feat: change PostgreSQL to MongoDB

--- source/backend/in-chat/src/main/java/com/chatrealtime/repository/UserRepository.java
+++ source/backend/in-chat/src/main/java/com/chatrealtime/repository/UserRepository.java
@@ -1,14 +1,14 @@
 package com.chatrealtime.repository;
 
 import com.chatrealtime.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.List;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, String> {
+public interface UserRepository extends MongoRepository<User, String> {
     boolean existsByUsername(String username);
     boolean existsByEmail(String email);
     Optional<User> findByUsername(String username);
